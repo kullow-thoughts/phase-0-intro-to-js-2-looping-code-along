3b50cdd2e45c56678cae495c6facdbad5cea8d1d
Abdulhakim Kullow <abdulhakim.kullow@student.moringaschool.com>
2024-03-28 18:33:20 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let nameArr = []
+function writeCards(names, event){
+    for(let i = 0; i < names.length; i++){
+        nameArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return nameArr
+}
+
+console.log(writeCards(nameArr, 'birthday'))
+
+function countDown(){
+    let count = 0
+    while(count <= 10){
+        console.log(count++)
+    }
+}
+countDown()
